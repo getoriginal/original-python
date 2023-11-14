04edfc1b82c67fdf276628cb58ea20be7b2e94e8
avmturo <30347978+avmturo@users.noreply.github.com>
2023-11-14 18:34:28 +0000

    Breaking: asyncio testing

---
+++ original/tests/async_chat/conftest.py
@@ -0,0 +1,45 @@
+import asyncio
+import os
+
+import pytest
+
+from original.async_original import OriginalAsync
+
+
+def pytest_runtest_makereport(item, call):
+    if "incremental" in item.keywords:
+        if call.excinfo is not None:
+            parent = item.parent
+            parent._previousfailed = item
+
+
+def pytest_runtest_setup(item):
+    if "incremental" in item.keywords:
+        previousfailed = getattr(item.parent, "_previousfailed", None)
+        if previousfailed is not None:
+            pytest.xfail(f"previous test failed ({previousfailed.name})")
+
+
+def pytest_configure(config):
+    config.addinivalue_line("markers", "incremental: mark test incremental")
+
+
+@pytest.fixture(scope="module")
+def event_loop():
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest.fixture(scope="module")
+async def client():
+    base_url = os.environ.get("TEST_ORIGINAL_HOST")
+    options = {"base_url": base_url} if base_url else {}
+    async with OriginalAsync(
+        api_key=os.environ["TEST_API_KEY"],
+        api_secret=os.environ["TEST_API_SECRET"],
+        timeout=10,
+        **options,
+    ) as original_client:
+        yield original_client
+
---
+++ original/tests/async_chat/test_client_e2e.py
@@ -0,0 +1,228 @@
+import os
+import time
+
+from dotenv import load_dotenv
+
+from original.async_original import OriginalAsync
+from original.tests.utils import get_random_string
+
+load_dotenv()
+
+TEST_APP_USER_EMAIL = os.getenv('TEST_APP_USER_EMAIL')
+TEST_APP_USER_UID = os.getenv('TEST_APP_USER_UID')
+TEST_APP_USER_CLIENT_ID = os.getenv('TEST_APP_USER_CLIENT_ID')
+TEST_APP_COLLECTION_UID = os.getenv('TEST_APP_COLLECTION_UID')
+TEST_ASSET_UID = os.getenv('TEST_ASSET_UID')
+TEST_TRANSFER_TO_WALLET_ADDRESS = os.getenv('TEST_TRANSFER_TO_WALLET_ADDRESS')
+TEST_TRANSFER_TO_USER_UID = os.getenv('TEST_TRANSFER_TO_USER_UID')
+
+
+class TestClientE2E:
+
+    async def test_create_user(self, client: OriginalAsync):
+        client_id = get_random_string(8)
+        response = await client.create_user(email=f"{client_id}@test.com", client_id=client_id)
+        assert response["data"]["uid"] is not None
+
+    async def test_get_user(self, client: OriginalAsync):
+        response = await client.get_user(TEST_APP_USER_UID)
+        assert response["data"]["uid"] == TEST_APP_USER_UID
+        assert response["data"]["email"] == TEST_APP_USER_EMAIL
+
+    async def test_get_user_by_email(self, client: OriginalAsync):
+        response = await client.get_user_by_email(TEST_APP_USER_EMAIL)
+        assert response["data"]["uid"] == TEST_APP_USER_UID
+        assert response["data"]["email"] == TEST_APP_USER_EMAIL
+
+    async def test_get_user_by_client_id(self, client: OriginalAsync):
+        response = await client.get_user_by_client_id(TEST_APP_USER_CLIENT_ID)
+        assert response["data"]["uid"] == TEST_APP_USER_UID
+        assert response["data"]["email"] == TEST_APP_USER_EMAIL
+
+    async def test_get_user_by_client_id_with_no_results(self, client: OriginalAsync):
+        response = await client.get_user_by_client_id("no_results")
+        assert response["data"] == None
+
+    async def test_get_user_not_found_throws_404(self, client: OriginalAsync):
+        try:
+            await client.get_user("not_found")
+        except Exception as e:
+            assert e.status_code == 404
+
+    async def test_get_collection(self, client: OriginalAsync):
+        response = await client.get_collection(TEST_APP_COLLECTION_UID)
+        assert response["data"]["uid"] == TEST_APP_COLLECTION_UID
+
+    async def test_get_collection_not_found_throws_404(self, client: OriginalAsync):
+        try:
+            await client.get_collection("not_found")
+        except Exception as e:
+            assert e.status_code == 404
+
+    async def test_create_asset(self, client: OriginalAsync):
+        asset_name = get_random_string(8)
+        asset_data = {
+            "name": asset_name,
+            "unique_name": True,
+            "image_url": "https://example.com/image.png",
+            "store_image_on_ipfs": False,
+            "attributes": [
+                {"trait_type": "Eyes", "value": "Green"},
+                {"trait_type": "Hair", "value": "Black"},
+            ],
+        }
+        request_data = {
+            "data": asset_data,
+            "user_uid": TEST_APP_USER_UID,
+            "client_id": asset_name,
+            "collection_uid": TEST_APP_COLLECTION_UID,
+        }
+        response = await client.create_asset(**request_data)
+        assert response["data"]["uid"] is not None
+
+    async def test_edit_asset(self, client: OriginalAsync):
+        asset_name = get_random_string(8)
+        asset_data = {
+            "name": asset_name,
+            "unique_name": True,
+            "image_url": "https://example.com/image.png",
+            "store_image_on_ipfs": False,
+            "description": "Asset description",
+            "attributes": [
+                {"trait_type": "Eyes", "value": "Green"},
+                {"trait_type": "Hair", "value": "Black"},
+            ],
+        }
+        request_data = {
+            "data": asset_data,
+            "user_uid": TEST_APP_USER_UID,
+            "client_id": asset_name,
+            "collection_uid": TEST_APP_COLLECTION_UID,
+        }
+        asset_response = await client.create_asset(**request_data)
+        asset_uid = asset_response["data"]["uid"]
+        is_transferable = False
+        retries = 0
+
+        while is_transferable is False and retries < 10:
+            response = await client.get_asset(asset_uid)
+            is_transferable = response["data"]["is_transferable"]
+            time.sleep(15)
+            retries += 1
+
+        assert is_transferable is True
+        edited_data = {"data": {**asset_data, "description": "Asset description edited"}}
+        edited_response = await client.edit_asset(asset_uid, **edited_data)
+        assert edited_response["success"] is True
+
+    async def test_get_asset(self, client: OriginalAsync):
+        response = await client.get_asset(TEST_ASSET_UID)
+        assert response["data"]["uid"] == TEST_ASSET_UID
+
+    async def test_get_asset_not_found_throws_404(self, client: OriginalAsync):
+        try:
+            await client.get_asset("not_found")
+        except Exception as e:
+            assert e.status_code == 404
+
+    async def test_get_asset_by_user_uid(self, client: OriginalAsync):
+        response = await client.get_assets_by_user_uid(TEST_APP_USER_UID)
+        assert isinstance(response["data"], list)
+
+    async def test_get_asset_by_user_uid_with_no_results(self, client: OriginalAsync):
+        response = await client.get_assets_by_user_uid("no_results")
+        assert response["data"] == []
+
+    async def test_get_transfer_by_user_uid(self, client: OriginalAsync):
+        response = await client.get_transfers_by_user_uid(TEST_APP_USER_UID)
+        assert isinstance(response["data"], list)
+
+    async def test_get_burn_by_user_uid(self, client: OriginalAsync):
+        response = await client.get_burns_by_user_uid(TEST_APP_USER_UID)
+        assert isinstance(response["data"], list)
+
+    async def test_full_create_transfer_burn_asset_flow(self, client: OriginalAsync):
+        asset_name = get_random_string(8)
+        asset_data = {
+            "name": asset_name,
+            "unique_name": True,
+            "image_url": "https://example.com/image.png",
+            "store_image_on_ipfs": False,
+            "description": "Asset description",
+            "attributes": [
+                {"trait_type": "Eyes", "value": "Green"},
+                {"trait_type": "Hair", "value": "Black"},
+            ],
+        }
+        request_data = {
+            "data": asset_data,
+            "user_uid": TEST_APP_USER_UID,
+            "client_id": asset_name,
+            "collection_uid": TEST_APP_COLLECTION_UID,
+        }
+        asset_response = await client.create_asset(**request_data)
+        asset_uid = asset_response["data"]["uid"]
+        is_transferable = False
+        retries = 0
+
+        while is_transferable is False and retries < 10:
+            response = await client.get_asset(asset_uid)
+            is_transferable = response["data"]["is_transferable"]
+            time.sleep(15)
+            retries += 1
+
+        assert is_transferable is True
+        transfer_response = await client.create_transfer(
+            asset_uid=asset_uid,
+            from_user_uid=TEST_APP_USER_UID,
+            to_address=TEST_TRANSFER_TO_WALLET_ADDRESS,
+        )
+        assert transfer_response["success"] is True
+        transfer_uid = transfer_response["data"]["uid"]
+        is_transferring = True
+        retries = 0
+
+        while is_transferring is True and retries < 10:
+            response = await client.get_asset(asset_uid)
+            is_transferring = response["data"]["is_transferring"]
+            time.sleep(15)
+            retries += 1
+
+        transfer_response = await client.get_transfer(transfer_uid)
+        assert transfer_response["success"] is True
+        assert transfer_response["data"]["status"] == 'done'
+
+        transferred_asset = await client.get_asset(asset_uid)
+        assert transferred_asset["data"]["is_transferable"] is True
+
+        burn_response = await client.create_burn(
+            asset_uid=asset_uid,
+            from_user_uid=TEST_TRANSFER_TO_USER_UID,
+        )
+        assert burn_response["success"] is True
+        burn_uid = burn_response["data"]["uid"]
+        is_burning = True
+        retries = 0
+
+        while is_burning is True and retries < 10:
+            response = await client.get_burn(burn_uid)
+            is_burning = response["data"]["status"] != 'done'
+            time.sleep(15)
+            retries += 1
+
+        burn_response = await client.get_burn(burn_uid)
+        assert burn_response["success"] is True
+        assert burn_response["data"]["status"] == 'done'
+
+        final_asset_burned_status = False
+        retries = 0
+
+        while final_asset_burned_status is False and retries < 10:
+            response = await client.get_asset(asset_uid)
+            final_asset_burned_status = response["data"]["is_burned"]
+            time.sleep(15)
+            retries += 1
+
+        final_asset = await client.get_asset(asset_uid)
+        assert final_asset["success"] is True
+        assert final_asset["data"]["is_burned"] is True
